3adacd45a2cf18a9e1fe5a7e8dd2f3f003310d44
Barbato Federico <barbato.federico01@outlook.com>
2020-01-07 23:14:18 +0100

    Rimozione Metodi navigazione pagine web

--- AppAndroid/Source/TerzoPrototipo/TourCasaEnergia/app/src/main/java/com/leviponti/tourcasaenergia/MainActivity.java
+++ AppAndroid/Source/TerzoPrototipo/TourCasaEnergia/app/src/main/java/com/leviponti/tourcasaenergia/MainActivity.java
@@ -145,31 +145,6 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
-    /**
-     *
-     * Metodo che permette di visualizzare sulla webview il sito precedente a quello corrente
-     *
-     * @param view
-     */
-    public void getPrevSite(View view){
-
-        if(this.webView.canGoBack()){
-            this.webView.loadUrl((webView.copyBackForwardList().getItemAtIndex(webView.copyBackForwardList().getCurrentIndex()-1)).getUrl());
-        }
-
-    }
-
-    /**
-     *
-     * Metodo che permette di visualizzare sulla webview il sito successivo a quello corrente
-     *
-     * @param view
-     */
-    public void getForwardSite(View view){
-        if(this.webView.canGoForward()){
-            this.webView.loadUrl((webView.copyBackForwardList().getItemAtIndex(webView.copyBackForwardList().getCurrentIndex()+1)).getUrl());
-        }
-    }
 
     /**
      *
@@ -222,7 +197,9 @@ public class MainActivity extends AppCompatActivity{
      */
     public void startScan(View view){
 
+
         try {
+
             sensoroManager.startService();
         } catch (Exception e) {
             e.printStackTrace();
